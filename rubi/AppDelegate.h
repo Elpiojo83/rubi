dc4fd164e3addbf41428e6eea2872f1949ca08d1
Elpiojo83 <david.krachler@gmail.com>
2014-03-27 08:18:33 +0100

    Initial Commit

---
+++ rubi/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  rubi
+//
+//  Created by David Krachler on 27.03.14.
+//  Copyright (c) 2014 koerbler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ rubi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rubi
+//
+//  Created by David Krachler on 27.03.14.
+//  Copyright (c) 2014 koerbler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
